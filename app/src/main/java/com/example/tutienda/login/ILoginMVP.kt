292021431c9ad00cfbc9303c387935cf7a8e7665
Edwin <edwinacubillos@gmail.com>
2019-03-26 19:46:45 -0500

    Revert "Feature login"

--- app/src/main/java/com/example/tutienda/login/ILoginMVP.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tutienda.login
-
-interface ILoginMVP {
-}
--- app/src/main/java/com/example/tutienda/register/IRegisterRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tutienda.register
-
-import android.net.Uri
-import com.example.tutienda.register.model.User
-
-interface IRegisterRepository {
-    fun createUser(user: User, urlPhoto: Uri)
-    fun savePhoto(urlPhoto: Uri, user: User)
-}
